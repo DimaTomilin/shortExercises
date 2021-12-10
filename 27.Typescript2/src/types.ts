ae8b08bd61eb1937e3ec2a405c463bef2f9765c9
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-12-10 08:07:46 +0200

    Change data and types

--- 27.Typescript2/src/types.ts
+++ 27.Typescript2/src/types.ts
@@ -3,7 +3,47 @@ export enum Gender {
   Female = 'female',
 }
 
-export interface Entry {}
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface Diagnose {
   code: string;
