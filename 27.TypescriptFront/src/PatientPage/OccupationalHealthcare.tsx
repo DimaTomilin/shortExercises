3fc99430a2ff66ccac1f578b5faff36340f53693
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-12-10 08:08:17 +0200

    Add types of entries

---
+++ 27.TypescriptFront/src/PatientPage/OccupationalHealthcare.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Icon, Card } from 'semantic-ui-react';
+import { OccupationalHealthcareEntry } from '../types';
+
+const style = { margin: 10 };
+
+const OccupationalHealthcare: React.FC<{
+  entry: OccupationalHealthcareEntry;
+}> = ({ entry }) => (
+  <div>
+    <Card style={style}>
+      <Card.Content>
+        {entry.date} <Icon name="stethoscope" />
+        {entry.employerName}
+      </Card.Content>
+      <Card.Content description={entry.description} />
+    </Card>
+  </div>
+);
+
+export default OccupationalHealthcare;
